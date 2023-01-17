5ba2c5dd1143ca6e7fdc48da1130f2264b7138be
Erin Chambers <echambers4@unl.edu>
2023-01-17 12:59:38 -0600

    Replace old events scripts with 5.0 plugins

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,10 @@
-// Load WDN Events plugins
-WDN.initializePlugin('monthwidget', {url: 'https://events.unl.edu/writing/'}); 
-WDN.initializePlugin('events', {url: 'https://events.unl.edu/writing/', limit:6});
-WDN.setPluginParam('events', 'href', '//events.unl.edu/writing/');
-WDN.setPluginParam('events', 'title', 'Writing Center');
+// Load 5.0 WDN Events plugins
+
+WDN.initializePlugin('events', {
+    url: 'https://events.unl.edu/writing/', 
+    container:'#wdn_calendarDisplay', limit: 3
+}); 
+
+WDN.initializePlugin('monthwidget', {
+    url: 'https://events.unl.edu/writing/'
+}); 
